3193f091b7a79b166400ae06f9f03ec86fbc85ed
xiangyu <xiangyu_975@163.com>
2020-09-24 16:43:19 +0800

    项目开发功能拓展之——短信验证码完

--- meiduo_mall/meiduo_mall/apps/verifications/views.py
+++ meiduo_mall/meiduo_mall/apps/verifications/views.py
@@ -1,14 +1,60 @@
 from django import http
-
+import random, logging
 from django.shortcuts import render
 from django.views import View
 from django_redis import get_redis_connection
 
+
+from meiduo_mall.utils.response_code import RETCODE
 from verifications import constants
 from verifications.libs.captcha.captcha import captcha
 
+from verifications.libs.yuntongxun.ccp_sms import CCP
 
 # Create your views here.
+
+
+# 创建日志输出器
+logger = logging.getLogger('django')
+
+
+class SMSCodeView(View):
+    '''短信验证码'''
+
+    def get(self, request, mobile):
+        '''
+        :param request: 请求对象
+        :param mobile: 手机号
+        :return: JSON
+        '''
+        # 接受参数
+        image_code_client = request.GET.get('image_code')
+        uuid = request.GET.get('uuid')
+        # 校验参数
+        if not all([mobile, image_code_client, uuid]):
+            return http.HttpResponseForbidden('缺少必传参数')
+        # 提取图形验证码
+        redis_conn = get_redis_connection('verify_code')
+        image_code_server = redis_conn.get('img_%s' % uuid)
+        if image_code_server is None:
+            return http.JsonResponse({'code': RETCODE.IMAGECODEERR, 'errmsg': '图形验证码已失效'})
+        # 删除图形验证码
+        redis_conn.delete('img_%s' % uuid)
+        # 对比图形验证码
+        image_code_server = image_code_server.decode()  # 将bytes转字符串，再比较
+        if image_code_client.lower() != image_code_server.lower():  # 转小写再比较
+            return http.JsonResponse({'code': RETCODE.IMAGECODEERR, 'errmsg': '输入的图形验证码有误'})
+        # 生成短信验证码：随机6位数字
+        sms_code = '%06d' % random.randint(0, 999999)
+        logger.info(sms_code)  # 手动的输出日志，记录短信验证码
+        # 保存短信验证码
+        redis_conn.setex('sms_%s' % mobile, constants.SMS_CODE_REDIS_EXPIRES, sms_code)
+        # 发送短信验证码
+        CCP().send_template_sms(mobile, [sms_code, constants.SMS_CODE_REDIS_EXPIRES // 60],
+                                constants.SEND_SMS_TEMPLATE_ID)
+        # 响应结果
+        return http.JsonResponse({'code': RETCODE.OK, 'errmsg': '发送短信成功'})
+
 
 class ImageCodeView(View):
     '''图形验证码'''
--- meiduo_mall/meiduo_mall/static/js/register.js
+++ meiduo_mall/meiduo_mall/static/js/register.js
@@ -13,6 +13,8 @@ let vm = new Vue({
         image_code_url: '',
         uuid: '',
         image_code: '',
+        sms_code_tip: '获取短信验证码',
+        send_flag: false,
 
         // v-show
         error_name: false,
@@ -21,17 +23,67 @@ let vm = new Vue({
         error_mobile: false,
         error_allow: false,
         error_image_code: false,
+        error_sms_code: false,
 
         // error_message
         error_name_message: '',
         error_mobile_message: '',
         error_image_code_message: '',
+        error_sms_code_message: '',
     },
     mounted(){ // 页面加载完会被调用
         // 生成图形验证码
         this.generate_image_code();
     },
     methods: {  // 定义和实现事件方法
+        // 发送短信验证码
+        send_sms_code(){
+            // 避免恶意用户频繁点击获取短信验证码标签
+            if (this.send_flag == true) { // 先判断是否有人在使用
+                return;
+            }
+            this.send_flag = true; // 如果可以进去，立即上锁
+            // 校验数据：mobile , image_code
+            this.check_mobile();
+            this.check_image_code();
+            if (this.error_mobile == true || this.error_image_code == true) {
+                this.send_flag = false;
+                return;
+            }
+            // 校验数据：mobile , image_code
+            let url = '/sms_codes/' + this.mobile + '/?image_code=' + this.image_code + '&uuid=' + this.uuid;
+            axios.get(url, {
+                responseType: 'json',
+            })
+                .then(response => {
+                    if (response.data.code == '0') {
+                        // 展示倒计时60秒
+                        let num = 60;
+                        let t = setInterval(() => {
+                            if (num == 1) {  // 倒计时即将结束
+                                clearInterval(t); // 停止回调函数的执行
+                                this.sms_code_tip = '获取短信验证码'; // 还原sms_code_tip的提示文字
+                                this.generate_image_code(); // 重新生成图形验证码
+                                this.send_flag = false;
+                            } else {
+                                num -= 1;  // num = num-1
+                                this.sms_code_tip = num + '秒';
+                            }
+
+                        }, 1000)
+                    } else {
+                        if (response.data.code == '4001') { // 图形验证码错误
+                            this.error_image_code_message = response.data.errmsg;
+                            this.error_image_code = true;
+                        }
+                        this.send_flag = false;
+                    }
+                })
+                .catch(error => {
+                    console.log(error.response);
+                    this.send_flag = false;
+                })
+        },
         // 生成图形验证吗的方法：封装的思想，代码复用
         generate_image_code(){
             this.uuid = generateUUID();
